cf6d97ded737fc1903609e0b030781aa4e8b79df
Arden <ardencristophermendoza@gmail.com>
2022-10-06 20:07:20 +0800

    Added page title using Head Component from next.js added padding on top of page container

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import { ImageList } from "../components/ImageList/ImageList";
 import { Radio, Button, Modal, Input } from "antd";
 import { FileUploaderComponent as FileUploader } from "../components/FileUploaderButton/FileUploaderButton";
 import images from "../mockData";
+import Head from "next/head";
 
 const Home: NextPage = () => {
   const [isModalOpen, setIsModalOpen] = React.useState(false);
@@ -14,35 +15,40 @@ const Home: NextPage = () => {
   const [imgs, setImgs] = React.useState(images);
 
   return (
-    <div className={styles.indexPageContainer}>
-      <FileUploader
-        isModalOpen={isModalOpen}
-        setIsModalOpen={setIsModalOpen}
-        onUploadSuccess={(image) => {
-          return setImgs([
-            ...imgs,
-            {
-              name: image.name,
-              description: image.description,
-              date: new Date().toString(),
-              imgLoc: image.file?.thumbUrl ?? "",
-            },
-          ]);
-        }}
-      />
-      <div>
-        {"Sort by"}
-        <Radio.Group
-          className={styles.sortGroup}
-          onChange={(e) => setSortBy(e.target.value)}
-          defaultValue={"name"}
-        >
-          <Radio.Button value={"name"}>{"Name"}</Radio.Button>
-          <Radio.Button value={"date"}>{"Date"}</Radio.Button>
-        </Radio.Group>
+    <>
+      <Head>
+        <title>{"Image Catalog"}</title>
+      </Head>
+      <div className={styles.indexPageContainer}>
+        <FileUploader
+          isModalOpen={isModalOpen}
+          setIsModalOpen={setIsModalOpen}
+          onUploadSuccess={(image) => {
+            return setImgs([
+              ...imgs,
+              {
+                name: image.name,
+                description: image.description,
+                date: new Date().toString(),
+                imgLoc: image.file?.thumbUrl ?? "",
+              },
+            ]);
+          }}
+        />
+        <div>
+          {"Sort by"}
+          <Radio.Group
+            className={styles.sortGroup}
+            onChange={(e) => setSortBy(e.target.value)}
+            defaultValue={"name"}
+          >
+            <Radio.Button value={"name"}>{"Name"}</Radio.Button>
+            <Radio.Button value={"date"}>{"Date"}</Radio.Button>
+          </Radio.Group>
+        </div>
+        <ImageList images={imgs} sortBy={sortBy} />
       </div>
-      <ImageList images={imgs} sortBy={sortBy} />
-    </div>
+    </>
   );
 };
 
